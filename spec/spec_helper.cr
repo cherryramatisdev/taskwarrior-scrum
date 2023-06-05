e9e5696c3b59b8d2351aec0bc18d288179773a58
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-06-05 20:42:45 -0300

    chore: rename binary name

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/taskwarrior-gtd"
+require "../src/taskwarrior-scrum"
--- src/taskwarrior-gtd.cr
+++
@@ -1,6 +0,0 @@
-# TODO: Write documentation for `Taskwarrior::Gtd`
-module Taskwarrior::Gtd
-  VERSION = "0.1.0"
-
-  # TODO: Put your code here
-end
