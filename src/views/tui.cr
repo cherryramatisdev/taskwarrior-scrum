1cfceab9a7d932e6402668dd65e7ccaab593c7f9
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-06-09 21:11:37 -0300

    feat: add time spent on a task as a new column

--- src/views/tui.cr
+++ src/views/tui.cr
@@ -3,24 +3,40 @@ require "tallboy"
 class Tui
   alias TaskWarriorData = Array(Hash(String, String))
 
-  def self.new(data : TaskWarriorData)
-    table = Tallboy.table do
+  @table : Tallboy::TableBuilder?
+
+  def initialize(data : TaskWarriorData)
+    @table = Tallboy.table do
       columns do
         add "description"
         add "status"
         add "uuid"
         add "created_at"
+        add "ended_at"
+        add "time spent"
       end
 
       header
 
-      rows Tui.rows(data)
+      rows self.rows(data)
     end
   end
 
-  def self.rows(data : TaskWarriorData) : Array(Array(String))
+  def table
+    @table
+  end
+
+  private def rows(data : TaskWarriorData) : Array(Array(String))
     data.map do |task|
-      [task["description"], task["status"], task["uuid"], Time.unix(task["entry"].to_i).to_s("%d/%m/%Y %H:%M")]
+      [task["description"], task["status"], task["uuid"], self.unix_time_to_date(task["entry"].to_i), self.unix_time_to_date(task["end"].to_i), self.unix_time_to_time(task["end"].to_i - task["entry"].to_i)]
     end
+  end
+
+  private def unix_time_to_date(unix_time : Int32) : String
+    Time.unix(unix_time).to_s("%d/%m/%Y %H:%M")
+  end
+
+  private def unix_time_to_time(unix_time : Int32) : String
+    Time.unix(unix_time).to_s("%H:%M")
   end
 end
